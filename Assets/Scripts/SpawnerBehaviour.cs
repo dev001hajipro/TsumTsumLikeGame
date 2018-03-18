4bd31cfce6e95bb8f88c53e9099845f479075089
dev001hajipro <dev001hajipro@gmail.com>
2018-03-18 14:04:49 +0900

    add sprites

---
+++ Assets/Scripts/SpawnerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehaviour : MonoBehaviour
+{
+	public GameObject ballPrefab;
+
+	void Start ()
+	{
+		StartCoroutine (Spawn (5));
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	IEnumerator Spawn (int count)
+	{
+		yield return new WaitForSeconds (1f);
+	}
+}
