1572318a8fcec61ddff51b2d492a398fae54cc1b
dev001hajipro <dev001hajipro@gmail.com>
2018-03-19 20:42:57 +0900

    ボールを消せるようになった

---
+++ Assets/Scripts/AnimalBallBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimalBallBehaviour : MonoBehaviour,
+    IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler
+{
+    private SelectedTargetManager mgr;
+
+    private void Awake()
+    {
+        mgr = GameObject.Find("SelectedTargetManager").GetComponent<SelectedTargetManager>();
+    }
+
+    #region IPointer Handlers
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Reset Mgr.List");
+        mgr.Clear();
+        mgr.IsSelecting = true;
+        if (mgr.IsValid(this.gameObject))
+        {
+            Debug.Log("Added gameobject.");
+            mgr.Add(this.gameObject);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter");
+        if (mgr.IsSelecting)
+        {
+            Debug.Log("OnPointerEnter isSelecting");
+            if (mgr.IsValid(this.gameObject))
+            {
+                Debug.Log("Added gameobject.");
+                mgr.Add(this.gameObject);
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (mgr.IsScoreing())
+        {
+            // もし妥当なら削除して得点。
+            mgr.CalcPoint();
+            mgr.DestroyAnimalBalls();
+            Debug.Log("DestroyAnimalBalls!!");
+        }
+        mgr.Clear();
+        mgr.IsSelecting = false;
+    }
+    #endregion
+}
