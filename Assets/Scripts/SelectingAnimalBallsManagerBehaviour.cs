f6b3560b199c8c985630b259cf9e74fc4fbb323d
dev001hajipro <dev001hajipro@gmail.com>
2018-03-20 17:46:35 +0900

    選択中の透過処理

--- Assets/Scripts/SelectingAnimalBallsManagerBehaviour.cs
+++ Assets/Scripts/SelectingAnimalBallsManagerBehaviour.cs
@@ -5,38 +5,52 @@ using System.Linq;
 
 public class SelectingAnimalBallsManagerBehaviour : MonoBehaviour {
 
-    private List<GameObject> removeTargetObjects;
+    private List<GameObject> selectingBalls;
 
     public bool IsSelecting { get; set; }
 
     void Awake()
     {
-        removeTargetObjects = new List<GameObject>();
+        selectingBalls = new List<GameObject>();
     }
 
     private void Init()
     {
-        removeTargetObjects.Clear();
+        selectingBalls.ForEach(o => DoTransparency(o, 1f));
+        selectingBalls.Clear();
         IsSelecting = false;
     }
 
     private void Add(GameObject item)
     {
-        if (!removeTargetObjects.Contains(item))
-            removeTargetObjects.Add(item);
+        if (!selectingBalls.Contains(item))
+            selectingBalls.Add(DoTransparency(item, 0.5f));
     }
     private bool IsValid(GameObject item)
     {
-        if (removeTargetObjects.Count == 0) return true;
+        if (selectingBalls.Count == 0) return true;
         var mySpriteName = item.GetComponent<SpriteRenderer>().sprite.name;
-        return removeTargetObjects.All(o => o.GetComponent<SpriteRenderer>().sprite.name.Equals(mySpriteName));
+        return selectingBalls.All(o => o.GetComponent<SpriteRenderer>().sprite.name.Equals(mySpriteName));
     }
 
-    private bool IsScoreing() => IsSelecting && removeTargetObjects.Count >= 3;
+    private bool IsScoreing() => IsSelecting && selectingBalls.Count >= 3;
 
-    private int CalcPoint() => removeTargetObjects.Count * 10;
+    private int CalcPoint() => selectingBalls.Count * 10;
 
-    private void DestroyAnimalBalls() => removeTargetObjects.ForEach(o => Destroy(o));
+    private void DestroyAnimalBalls()
+    {
+        selectingBalls.ForEach(o => Destroy(o));
+        selectingBalls.Clear();
+    }
+
+    // TODO animalballへ透過イベントを送り処理すべき?
+    private GameObject DoTransparency(GameObject o, float alpha = 1f)
+    {
+        Color c = o.GetComponent<SpriteRenderer>().color;
+        c.a = alpha;
+        o.GetComponent<SpriteRenderer>().color = c;
+        return o;
+    }
 
     public void StartSelecting(GameObject o)
     {
