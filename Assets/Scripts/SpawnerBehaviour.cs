f826b72165972f1508980ed55fb22f7d6a7c56f9
dev001hajipro <dev001hajipro@gmail.com>
2018-03-21 21:06:34 +0900

    add respawn button

--- Assets/Scripts/SpawnerBehaviour.cs
+++ Assets/Scripts/SpawnerBehaviour.cs
@@ -21,8 +21,16 @@ public class SpawnerBehaviour : MonoBehaviour
 
     private GameObject CreateBall()
     {
-        var o = Instantiate(ballPrefab, new Vector2(Random.Range(-2.0f, 2.0f), 7f), Quaternion.AngleAxis(Random.Range(-40, 40), Vector3.forward));
+        var o = Instantiate(ballPrefab, new Vector2(Random.Range(-2.0f, 2.0f), 7f), Quaternion.AngleAxis(Random.Range(-40, 40), Vector3.forward), gameObject.transform);
         o.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
         return o;
     }
+
+    public void ReSpawn()
+    {
+        DestroyChildren();
+        StartCoroutine(Spawn(50));
+    }
+
+    private void DestroyChildren() => transform.Cast<Transform>().ToList().ForEach(t => Destroy(t.gameObject));
 }
