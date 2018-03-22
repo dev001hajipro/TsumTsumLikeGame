cd08e7485ab31f764dcf1661f1bab836d2225afd
dev001hajipro <dev001hajipro@gmail.com>
2018-03-22 16:48:32 +0900

    timeout 実装

---
+++ Assets/Scripts/TimerTextBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TimerTextBehaviour : MonoBehaviour {
+
+    private Text timerText;
+    public float timer = 1f;
+
+	void Start () {
+        timerText = GetComponent<Text>();
+	}
+	
+	void Update () {
+        timer -= Time.deltaTime;
+
+        timerText.text = $"Time:{timer.ToString("00")}";
+        
+		if (timer < 0f)
+        {
+            Debug.Log("timer zero!!!");
+        }
+	}
+}
